4f439f8075295b8e78f1212ae19dd2a3f449a94c
Uhbifg <gri279@gmail.com>
2021-02-22 15:20:50 +0000

    try1

--- projects/1/model.py
+++ projects/1/model.py
@@ -1,7 +1,7 @@
 numeric_features = ["if"+str(i) for i in range(1,14)]
-categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
+#categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
 
-fields = ["id", "label"] + numeric_features + categorical_features
+fields = ["id", "label"] + numeric_features
 
 model = Pipeline(steps=[
     ('preprocessor', preprocessor),
