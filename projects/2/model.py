e4f1215f54e215ae1e47c7d063a784586bc824c8
Uhbifg <gri279@gmail.com>
2021-03-03 13:54:17 +0000

    fix model

--- projects/2/model.py
+++ projects/2/model.py
@@ -1,8 +1,35 @@
-numeric_features = ["if"+str(i) for i in range(1,14)]
-#categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import train_test_split, GridSearchCV
+from catboost import CatBoostRegressor
 
-fields = ["id", "label"] + numeric_features
+
+numeric_features = ["if"+str(i) for i in range(1,14)]
+categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
+
+fields = ["id", "label"] + numeric_features + categorical_features
+
+
+model_features = numeric_features
+
+train_features = ["id"] + numeric_features + ["cf"+str(i) for i in range(1,27)]
+
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median'))
+])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features)
+    ]
+)
+
 
 model = Pipeline(steps=[
-    ('linearregression', LinearRegression())
+    ('preprocessor', preprocessor),
+    ('linearregression', CatBoostRegressor(learning_rate=0.01, early_stopping_rounds=1000, iterations=4000))
 ])
+
