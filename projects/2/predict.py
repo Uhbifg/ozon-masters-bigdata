5cd94109ff6c760f06318df022c0c1453fa00648
Uhbifg <gri279@gmail.com>
2021-03-04 12:57:59 +0000

    new pred

--- projects/2/predict.py
+++ projects/2/predict.py
@@ -26,7 +26,8 @@ read_opts=dict(
 
 
 for df in pd.read_csv(sys.stdin, **read_opts):
-    X_test = df[model_features]
+    df.replace({'\N': None}, inplace=True)
+    X_test = df[model_features].astype('Int64', copy=False)
     pred = model.predict(X_test)
     out = zip(df.id, pred)
     print("\n".join(["{0}\t{1}".format(*i) for i in out]))
