92d5a54f088ebeb1225577aa7d4659807ffbe923
Uhbifg <gri279@gmail.com>
2021-03-30 00:05:58 +0000

    1

---
+++ projects/3/shortest_path.py
@@ -0,0 +1,55 @@
+import os
+import sys
+from pyspark import SparkContext, SparkConf
+
+
+conf = SparkConf()
+sc = SparkContext(appName="hw_3_Uhbifg", conf=conf)
+os.environ["PYTHONHASHSEED"] = "42"
+start_node = 1sys.argv[1]
+end_node = sys.argv[2]
+path_data = sys.argv[3]
+path_ans = sys.argv[4]
+
+raw_graph = sc.textFile(path_data)
+graph = raw_graph.map(lambda x: map(int, x.split("\t")[::-1])).cache()
+links = graph.groupByKey().mapValues(list).cache()     
+
+def search(x1, x2, x3):
+    ans = list()
+    a = x3[x1]
+    for i in a:
+        ans.append(tuple([i, x2 + [i]]))
+    return ans
+
+
+links = graph.groupByKey().mapValues(list).cache()     
+answers = []
+pathes = sc.parallelize([(start_node, [start_node])])
+
+
+while True:
+    ans = pathes.filter(lambda x : x[0] == end_node)
+    if ans.count() > 0:
+        break
+    else:
+        rdd2 = pathes.map(lambda x : x[0]).distinct().collect()
+        neigbours = dict() 
+        for i in rdd2:
+            temp = links.lookup(i)
+            if len(temp) > 0:
+                neigbours[i] = temp[0]
+            else:
+                neigbours[i] = []
+        rdd3 = sc.parallelize(pathes.flatMap(lambda x : search(x[0], x[1], neigbours)).filter(lambda x : 
+                                                                                             len(x[1]) == len(set(x[1]))).collect())
+        pathes = rdd3
+ans = ans.map(lambda x : x[1])
+
+def toCSVLine(data):
+    return ','.join(str(d) for d in data)
+
+lines = ans.map(toCSVLine)
+lines.saveAsTextFile(path_ans)
+
+sc.stop()
