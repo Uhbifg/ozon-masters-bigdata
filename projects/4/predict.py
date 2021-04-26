05b6b5e7e317cc95a04cce90da6f5498fad4ce93
Uhbifg <gri279@gmail.com>
2021-04-26 16:47:05 +0000

    a

---
+++ projects/4/predict.py
@@ -0,0 +1,36 @@
+#!/opt/conda/envs/dsenv/bin/python
+import os, sys
+import logging
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+
+spark = SparkSession.builder.getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
+
+from pyspark.ml import Pipeline, PipelineModel
+
+
+model_path = sys.argv[1]
+test_path = sys.argv[2]
+y_path = sys.argv[3]
+
+schema = StructType([
+    StructField("overall", FloatType()),
+    StructField("vote", StringType()),
+    StructField("verified", StringType()),
+    StructField("reviewTime", StringType()),
+    StructField("reviewerID", StringType()),
+    StructField("asin", StringType()),
+    StructField("reviewerName", StringType()),
+    StructField("reviewText", StringType()),
+    StructField("summary", StringType()),
+    StructField("unixReviewTime", StringType())
+])
+
+
+model = PipelineModel.load(model_path)
+test = spark.read.json(test_path, schema=schema)
+y = model.transform(test)
+predictions.select("id", "y").write.save(y_path)
+spark.stop()
---
+++ projects/4/train.py
@@ -0,0 +1,34 @@
+#!/opt/conda/envs/dsenv/bin/python
+import os, sys
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+
+spark = SparkSession.builder.getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
+
+from model import pipeline
+from pyspark.sql.types import *
+
+train_path = sys.argv[1]
+model_path = sys.argv[2]
+
+
+
+
+schema = StructType([
+    StructField("overall", FloatType()),
+    StructField("vote", StringType()),
+    StructField("verified", StringType()),
+    StructField("reviewTime", StringType()),
+    StructField("reviewerID", StringType()),
+    StructField("asin", StringType()),
+    StructField("reviewerName", StringType()),
+    StructField("reviewText", StringType()),
+    StructField("summary", StringType()),
+    StructField("unixReviewTime", StringType())
+])
+
+dataset = spark.read.json(train_path, schema=schema)
+dataset.cache()
+pipeline_model.write().overwrite().save(model_path)
+spark.stop()
