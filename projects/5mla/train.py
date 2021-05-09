2ba64754cb30268f553445442026d38fa43e1e7e
Uhbifg <gri279@gmail.com>
2021-05-09 21:25:17 +0000

    a

--- projects/5mla/train.py
+++ projects/5mla/train.py
@@ -1,12 +1,14 @@
-from catboost import CatBoostRegressor
 from sklearn.metrics import accuracy_score, log_loss
 from sklearn import preprocessing
 from sklearn.linear_model import LogisticRegression
 import os, sys
 import logging
-
-
+import mlflow
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 import pandas as pd
+
+from catboost import CatBoostRegressor
 
 if __name__ == "__main__":
     try:
@@ -18,37 +20,27 @@ if __name__ == "__main__":
 
     numeric_features = ["if"+str(i) for i in range(1,14)]
     categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
-    fields = ["id", "label"] + numeric_features + categorical_features
-    model_features = numeric_features + categorical_features + ["day_number"]
-    train_features = ["id"] + numeric_features + ["cf"+str(i) for i in range(1,27)]
+    fields = ["id", "label"] + numeric_features + categorical_features 
+    model_features = numeric_features + categorical_features 
 
 
-    mlflow.sklearn.autolog()
 
     read_table_opts = dict(sep="\t", names=fields, index_col=False, nrows=150)
     df = pd.read_table(train_path, **read_table_opts)
 
     X_train = df[model_features]
     y_train = df.label
-
-
+    X_train = X_train.fillna("0")
+    #df = df.applymap(str)
     with mlflow.start_run():
-        preprocessor = ColumnTransformer(
-        transformers=[
-            ('cat', preprocessing.LabelEncoder(), categorical_features)
-        ]
-    )
 
-        model = Pipeline(steps=[
-            ('preprocessor', preprocessor),
-            ('linearregression', LogisticRegression(random_state=model_param, max_iter=100))
-        ])
-        model.fit(X_train, y_train)
+        model = CatBoostRegressor(random_seed=model_param, iterations=1)
+        model.fit(X=X_train, y=y_train, cat_features=categorical_features)
         y_proba = model.predict(X_train)
         loss = log_loss(y_train, y_proba)
         mlflow.log_metric("log_loss", 100)
-        mlflow.log_param("model_param1", model_param1)
-
+        mlflow.log_param("model_param1", model_param)
+        mlflow.catboost.log_model(model, artifact_path="model")
 
 
 
